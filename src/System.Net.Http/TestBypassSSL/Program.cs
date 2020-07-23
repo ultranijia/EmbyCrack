9b8e40e5dff871f98603add879af46ccc1923003
YukiCoco <theblackrains@outlook.com>
2020-07-24 04:10:00 +0800

    添加修改后的代码

---
+++ src/System.Net.Http/TestBypassSSL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TestBypassSSL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            HttpClient httpClient = new HttpClient();
+            var result = httpClient.GetAsync("https://mb3admin.com/").Result;
+            Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
